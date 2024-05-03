18755e9fef2e5db17aa6350c30578007886a9d8d
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2024-05-03 13:42:21 +0200

    engine

--- proyectos/juegos/src/games/ahorcado/juegoahorcado.adb
+++ proyectos/juegos/src/games/ahorcado/juegoahorcado.adb
@@ -3,6 +3,11 @@ with HelpersJuegoAhorcado; use HelpersJuegoAhorcado;
 with UIJuegoAhorcado; use UIJuegoAhorcado;
 package body JuegoAhorcado is
 
+    function ACIERTA_CON_LA_LETRA(palabra: String; letra: Character; letras_usadas: String) return Boolean is
+    begin
+        return not(letra in letras_usadas) and Index(palabra, letra) /= 0;
+    end ACIERTA_CON_LA_LETRA;
+
     overriding function JUGAR_MANO(config: ConfiguracionJuegoAhorcado) return Partida.RESULTADO is
         palabras                : ListadoPalabras;
         --palabra_a_adivinar      : Unbounded_String; Opción 1
@@ -13,11 +18,11 @@ package body JuegoAhorcado is
         declare
             intentos_restantes      : Integer           := 6;
             letras_usadas           : Unbounded_String  := "";
-            caracteres_especiales   : String            := " -";
+            caracteres_especiales   : constant String   := " -";
 
-            palabra_a_adivinar      : constant String := ELEGIR_PALABRA_ALEATORIAMENTE(palabras); -- Opción 2
-            palabra_para_validar    : constant String := NORMALIZAR_PALABRA(palabra_a_adivinar);
-            palabra_enmascarada     : String          := ENMASCARAR(palabra_para_validar, letras_usadas, config.caracteres_especiales);
+            palabra_a_adivinar      : constant String   := ELEGIR_PALABRA_ALEATORIAMENTE(palabras); -- Opción 2
+            palabra_para_validar    : constant String   := NORMALIZAR_PALABRA(palabra_a_adivinar);
+            palabra_enmascarada     : String            := ENMASCARAR(palabra_para_validar, letras_usadas, caracteres_especiales);
 
             caracter_elegido        : Character;
         begin
@@ -26,24 +31,19 @@ package body JuegoAhorcado is
                 caracter_elegido := PEDIR_LETRA_AL_JUGADOR;
                 caracter_elegido := NORMALIZAR_CARACTER(caracter_elegido);
                 letras_usadas := letras_usadas & caracter_elegido;
-                -- Si la palabra tiene la letra: -> CONDICIONAL
-                    -- REENMASCARAR
-                    -- Si ya acertó todas las letras: GANADOR... Y SALGO
-                -- Si no la tiene:
-                    -- Le quito vida
-            
-                -- Cuando salgo del bucle:
-                    -- Muestro el estado final del juego.
+                if ACIERTA_CON_LA_LETRA(palabra_para_validar, caracter_elegido, letras_usadas) then
+                    palabra_enmascarada := ENMASCARAR(palabra_para_validar, letras_usadas, caracteres_especiales);
+                    if palabra_enmascarada = palabra_para_validar then
+                        ganador := Partida.RESULTADO.JUGADOR;
+                        exit;
+                    end if;
+                else
+                    intentos_restantes := intentos_restantes - 1;
+                end if;
             end loop;
+            MOSTRAR_ESTADO(intentos_restantes, palabra_enmascarada, letras_usadas);
         end;
         return ganador;
     end JUGAR_MANO;
 
 end JuegoAhorcado;
-
-
---    function ENMASCARAR(palabra: String; caracteres_usados: String; caracteres_especiales: String) return String;
---    function NORMALIZAR_PALABRA(palabra: String) return String;
---    function NORMALIZAR_CARACTER(letra: Character) return String;
---    function CARGAR_PALABRAS_DE_FICHERO(tematica: String) return ListadoPalabras;
---    function ELEGIR_PALABRA_ALEATORIAMENTE(palabras: ListadoPalabras) return String;
