9fea93cce2d4ac66c3b12e7332f641bff82d96ea
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2024-05-09 13:41:31 +0200

    pruebas Aunit

--- proyectos/juegos/tests/main_test.adb
+++ proyectos/juegos/tests/main_test.adb
@@ -6,10 +6,16 @@ procedure Main_Test is
 
    procedure Run is new AUnit.Run.Test_Runner(Players_Test_Suite.Suite_Todas_Las_Pruebas);
 
-   V_Reporter : AUnit.Reporter.Text.Reporter;
+   V_Reporter : AUnit.Reporter.Text.Text_Reporter;
 
 begin
 
    Run(V_Reporter);
+   -- El AUnit empieza a hacer mágia.
+   -- Aunit va a llamar él solito a la función Register_Tests de Player_Creation_Test_Cases
+   -- Ya que en Suite_Todas_Las_Pruebas la función del paquete Players_Test_Suite,
+   -- Le he dado un tipo de dato declarado en Player_Creation_Test_Cases
+   -- A esa función, nos pasa un dato de tipo: AUnit.Test_Case.Test_Case
+   -- Y a ese Test-Case le vamos registrando las rutinas de pruebas que queremos hacer.
 
 end Main_Test;
--- proyectos/juegos/tests/players/players_test_suite.adb
+++ proyectos/juegos/tests/players/players_test_suite.adb
@@ -5,14 +5,14 @@ with AUnit.Test_Suites; -- A un conjunto de pruebas que quiero ejecutar... pero 
 
 package body Players_Test_Suite is
 
-    V_CASO_DE_PRUEBA:           Player_Creation_Test_Cases.Player_Test_Case;
-    V_SUITE_CON_MIS_PRUEBAS:    AUnit.Test_Suites.Test_Suite;
+    V_CASO_DE_PRUEBA:           aliased Player_Creation_Test_Cases.Player_Test_Case;
+    V_SUITE_CON_MIS_PRUEBAS:    aliased AUnit.Test_Suites.Test_Suite;
+    -- aliased: para que no se pierda la referencia a la instancia
 
     function Suite_Todas_Las_Pruebas return AUnit.Test_Suites.Access_Test_Suite is
     begin
 
         AUnit.Test_Suites.Add_Test(V_SUITE_CON_MIS_PRUEBAS'Access, V_CASO_DE_PRUEBA'Access);
-
         return V_SUITE_CON_MIS_PRUEBAS'Access;
 
     end Suite_Todas_Las_Pruebas;
